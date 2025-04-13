1acd4a6162a6b0743b126b4de793dae84b9747de
Luis Caraballo <lcm7039@gmail.com>
2025-04-13 18:10:55 -0500

    mack

--- Validador.api/src/main/java/com/validaor/api/Config/WebConfig.java
+++ Validador.api/src/main/java/com/validaor/api/Config/WebConfig.java
@@ -9,7 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "http://localhost:3074", // local → meg
+                        "http://localhost:5173", // local → zafiro
+                        "http://10.0.1.249:7839", // test → zafiro
+                        "https://testingmeg.pharmaser.com.co", // test → meg
+                        "https://meg.pharmaser.com.co" // prod → meg
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
